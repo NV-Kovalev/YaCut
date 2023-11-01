a24afb85e70adf822755f40f34adc8c93fb352e5
atar1boy <atar1boy@yandex.ru>
2023-11-01 21:53:56 +0300

    settings

--- settings.py
+++ settings.py
@@ -3,9 +3,10 @@ import os
 
 class Config(object):
     JSON_AS_ASCII = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
 
 
-BASE_URL = os.getenv('HOST')
+BASE_URL = os.getenv('HOST', default='http://localhost/')
